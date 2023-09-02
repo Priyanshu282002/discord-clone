58ee4aa00038eb8ae302bd69dd8ba8674d850d59
Priyanshu282002 <priyanshutyagi29@gmail.com>
2023-09-02 18:43:54 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        'blue':'#404eed'
+      },
+      fontFamily:{
+        Whitney:["Whitney"],
+        Ginto:["Ginto"],
+        ggSans:["ggSans"],
+      },
+      
+    },
+  },
+  plugins: [],
+  
+}
